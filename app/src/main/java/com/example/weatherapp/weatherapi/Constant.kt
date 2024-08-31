54d978d728fe51ba49fc1c8dd96d1a2df09e6535
Gourav124 <gouravamolekar@gmail.com>
2024-08-31 11:37:32 +0530

    First Commit

---
+++ app/src/main/java/com/example/weatherapp/weatherapi/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapp.weatherapi
+
+object Constant {
+    val apiKey = "cbcbe083cd44492b8ef100840242608 "
+}
---
+++ app/src/main/java/com/example/weatherapp/weatherapi/NetworkResponse.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.weatherapi
+
+sealed class NetworkResponse<out T> { //T refers to WeatherModel
+    data class Success<out T>(val data : T):NetworkResponse<T>()
+    data class Error (val message : String) : NetworkResponse<Nothing>()
+    object Loading : NetworkResponse<Nothing>()
+}
